7fb315ca5256677b447e5750d52913fee4931cbb
Mohit <144538075+Mohit-Alive@users.noreply.github.com>
2024-08-20 08:43:57 +0530

    Create 03-circleArea.cpp

---
+++ 03-circleArea.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Circle{
+    float radius;
+    const float PI = 3.14;
+    
+    public:
+        static int count;
+        
+        Circle(){
+            radius = 0;
+        }
+
+        Circle(float r){
+            radius = r;
+        }
+        
+        float circleArea() const{
+            return PI*radius*radius;
+        }
+};
+
+int main() {
+    Circle obj(5);
+    cout<<obj.circleArea()<<endl;
+
+    return 0;
+}
